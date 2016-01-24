eb25f3a3ca01eff830bacff99018ccfc6e238345
Gihyuk Ko <fishhyuk11@gmail.com>
2016-01-24 05:12:57 +0000

    Minor change?

--- SoundHound/houndify_python_sdk-0.1.3/streamstdin.py
+++ SoundHound/houndify_python_sdk-0.1.3/streamstdin.py
@@ -9,11 +9,23 @@ import time
 if __name__ == '__main__':
 
 
-        inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL,'Device')
-        inp.setchannels(1)
-        inp.setrate(16000)
-        inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
-        inp.setperiodsize(2048)
+        inp1 = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL,'Device')
+        inp1.setchannels(1)
+        inp1.setrate(16000)
+        inp1.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+        inp1.setperiodsize(2048)
+
+        inp2 = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL, 'Device_1')
+        inp2.setchannels(1)
+        inp2.setrate(16000)
+        inp2.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+        inp2.setperiodsize(2048)
+
+        inp3 = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL, 'Device_2')
+        inp3.setchannels(1)
+        inp3.setrate(16000)
+        inp3.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+        inp3.setperiodsize(2048)
 
         print(alsaaudio.cards())
 
@@ -54,7 +66,7 @@ if __name__ == '__main__':
             i += 1
 #	    BUFFER_SIZE = 512
             l, data = inp.read()
-	   # samples = sys.stdin.read(BUFFER_SIZE)
+	    #samples = sys.stdin.read(BUFFER_SIZE)
 	    finished = False
 	    client.start(MyListener())
 	    while not finished:
